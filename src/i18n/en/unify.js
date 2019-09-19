0244a317b76059c59db02245dd651c5f18ffac17
Jože Fortun <joze.fortun@gmail.com>
2019-09-19 20:10:27 +0200

    Change Corteza branding to Crust

--- src/i18n/en/unify.js
+++ src/i18n/en/unify.js
@@ -5,5 +5,6 @@ const unify = one
 unify.header.appName = 'Crust'
 unify.header.appTag = 'Unify'
 unify.app.jitsi.defaultRemoteDisplayName = 'Fellow Cruster'
+unify.app.general.openTab = 'open in Crust Unify'
 
 export default unify

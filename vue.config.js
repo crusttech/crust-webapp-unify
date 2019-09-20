eb33b6e758c9826fdc2309863af7b3450310529d
Jože Fortun <joze.fortun@gmail.com>
2019-09-20 11:16:49 +0200

    Add appFlavour to build variables

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const buildVueConfig = require('corteza-webapp-one/vue.config-builder')
 
 module.exports = buildVueConfig({
+  appFlavour: 'Crust',
   appName: 'unify',
   appLabel: 'Crust Unify',
   theme: 'crust-base',
